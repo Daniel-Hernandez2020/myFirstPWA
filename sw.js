4c5dde7c109d0931c34cba0901381825643fbd19
Daniel <juanhg.up@gmail.com>
2022-11-27 21:43:49 -0800

    probando cambios

--- sw.js
+++ sw.js
@@ -23,11 +23,15 @@
  //offline
  self.addEventListener("fetch", e => {
 
+    //me parece que fecth es un evento del SW que trae toda la data de la aplicion desde donde tengas tu
+    //domino o si esta en local pues desde localhost, fetch trae archivos html,css, imagenes todos
+    //lo que tenga la pagina por si hay un cambio lo trae. Pero primero lo checa si esta en cache
+
     console.log("fetch", e.request)
 
     e.respondWith(
 
-        caches.match(e.request)//busca si existe un request como el que te mandare
+        caches.match(e.request)//busca si existe un request como el que te mandare ->compara si lo que esta solicitando ha cambiado con respecto a lo que tiene la cache
         .then( res => res || fetch(e.request))//devuelve de la cache si no devulve de la solicitud original que estabas haciendo
         .catch(console.log))
 
